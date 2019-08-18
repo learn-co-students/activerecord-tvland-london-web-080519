ebd28f4f83070b6594beecfc0ceaa7b46b6e35c4
github username <aude53@gmail.com>
2019-08-18 08:48:08 +0100

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,14 @@
 class Show < ActiveRecord::Base
-  
+    belongs_to :network
+    has_many :characters
+    has_many :actors, through: :characters
+
+    def actors_list
+        self.characters.map{|character| character.actor.full_name}
+    end
+
+    def build_network(attribute)
+        new_network = Network.new(attribute)
+        self.network = new_network
+    end
 end
